0ee5ff3264280de1dba5287d6ed92bee97293493
Martin Ortega <lynxestudio@outlook.com>
2023-11-09 17:35:51 -0600

    Add source code to the project.

---
+++ src/DiasSemana.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Samples.Basics.Enums
+{
+	public enum DiasSemana
+	{
+		Lunes = 1,
+		Martes = 2,
+		Miercoles = 3,
+		Jueves = 4,
+		Viernes = 5,
+		Sabado = 6,
+		Domingo = 7
+	}
+}
+
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Samples.Basics.Enums
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			Console.WriteLine(ImprimeDia(DiasSemana.Martes));
+			Console.WriteLine(ImprimeDia(DiasSemana.Sabado));
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			return 0;
+		}
+		
+		static string ImprimeDia(DiasSemana ds)
+		{
+			string s = null;
+			switch(ds)
+			{
+				case DiasSemana.Lunes:
+					s = "Tengo que levantarme temprano";
+					break;
+				case DiasSemana.Martes:
+					s = "Aún no me recupero";
+					break;
+				case DiasSemana.Miercoles:
+					s = "Faltan 2 días";
+					break;
+				case DiasSemana.Jueves:
+					s = "Falta un día solamente";
+					break;
+				case DiasSemana.Viernes:
+					s = "Por fin es viernes";
+					break;
+				case DiasSemana.Sabado:
+					s = "Es el mejor día de la semana";
+					break;
+				case DiasSemana.Domingo:
+					s = "Ya mañana es lunes de nuevo";
+					break;
+			}
+			return s;
+		}
+	}
+}
